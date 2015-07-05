5caa7e152b178c7119552ba01eda722b49f9a0e4
AllenShow <xiulunlin@live.com>
2015-07-05 13:24:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by allenshow on 7/4/15.
+ */
+
+var net=require('net');
+var Client=require('./lib/client');
+
+var Server=require('./lib/server');
+
+exports.createServer=function(options){
+    return new Server(options);
+}
+
+exports.createClient=function(options){
+    var socket=net.createConnection(options);
+    return new Client(socket);
+}
+
+exports.Client=Client;
---
+++ test/testserver.js
@@ -0,0 +1,33 @@
+var nrr = require('../index');
+
+//server
+
+var server = nrr.createServer();
+
+server.on('clientConnected', function (client) {
+    client.on('onRequest', function (payload, response){
+        console.log('Message from client: '+payload.toString());
+        //response
+        response('Okay, I am server.');
+    });
+});
+
+server.listen(8080);
+
+//client
+
+var client =new nrr.Client();
+
+client.connect(8080);
+
+client.on('connect', function () {
+
+    //send a string or buffer
+    this.send('Hello, I am client.', function (error, responsePayload){
+        if (error) {
+            console.log(error);
+            return;
+        }
+        console.log('Response from server: '+responsePayload.toString());
+    });
+})
